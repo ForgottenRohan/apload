ae72ba52160c50602d219c77bfe782b09a2f2ef2
Rohan <139545262+ForgottenRohan@users.noreply.github.com>
2025-01-31 22:58:22 +0500

    a

--- offbot/config.py
+++ offbot/config.py
@@ -1,5 +1,5 @@
 ﻿BOT_TOKEN = "7771154906:AAGOZnoze9DPFS8ddGQ71r0dAkuBqqQzqdI"  # токен ботика
-CHANNEL_URL = "https://t.me/lallsf"  # ссылка канала проверка подписки
+CHANNEL_URL = "https://t.me/millonerrrrrrr"  # ссылка канала проверка подписки
 CHANNEL_ID = "-1002072322896"  # айди канала для проверки подписки
 VERIF_CHANNEL_ID = "-1002435215340"  # айди чата для верификации(обсуждение канала с постбеками)
 SUPP = "https://t.me/supporw1n"
